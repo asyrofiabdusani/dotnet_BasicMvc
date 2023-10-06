fd004a0142e2ab6afcc947dade88851208dfb067
rofi <asyrofiabdusani@gmail.com>
2023-10-06 08:54:45 +0700

    initial init

---
+++ BasicMvc/Controllers/HomeController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BasicMvc.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            ViewBag.WhoAmi = "Customer";
+            ViewBag.TitlePage = "Index Page";
+            return View();
+        }
+        public ActionResult Products()
+        {
+            var products = new List<String>();
+            products.Add("Product 1");
+            products.Add("Product 2");
+            products.Add("Product 3");
+            ViewBag.products = products;
+
+            return View();
+        }
+        public ActionResult About()
+        {
+            ViewBag.Url = Request.Url;
+            ViewBag.PhysicApp = Request.PhysicalApplicationPath;
+            return View("Contacts");
+        }
+        public ActionResult Greet()
+        {
+            return Content("Hello guys");
+        }
+        public ActionResult TestShared()
+        {
+            return View("TestShared");
+        }
+    }
+}
---
+++ BasicMvc/Controllers/SecondPageController.cs
@@ -0,0 +1,17 @@
+﻿using System.Web.Mvc;
+
+namespace BasicMvc.Controllers
+{
+    public class SecondPageController : Controller
+    {
+        // GET: SecondPage
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult SecondTab()
+        {
+            return View("TestShared");
+        }
+    }
+}
